ef438bc49dced3381265b5297c05469be0fc3da8
Hariom Thakur <HARIKRDBG121@GMAIL.COM>
2026-03-27 10:58:07 +0530

    Update Billing.jsx

--- frontend/src/pages/Billing.jsx
+++ frontend/src/pages/Billing.jsx
@@ -4,7 +4,7 @@ import { QRCodeSVG } from 'qrcode.react'; // QR Code ke liye
 import { ReceiptText, ShoppingCart, Plus, Minus, Trash2, CheckCircle2 } from 'lucide-react';
 import { motion } from 'framer-motion';
 
-const API_BASE = 'https://gopal-ji-general-store.vercel.app/api';
+const API_BASE = 'shop-dashboard-pi.vercel.app/api';
 
 const Billing = () => {
   const [products, setProducts] = useState([]);
